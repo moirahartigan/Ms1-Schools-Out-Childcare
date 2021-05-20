f1485425d50be21718a559adbc26900b9c103ac1
moirahartigan <moirahartigan@gmail.com>
2021-05-20 13:10:44 +0000

    Add js file to assets and link script for nav bar toggle

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+
+//  JavaScript for Toggle menu - https://stackoverflow.com/questions/27577211/javascript-toggle-menu
+  
+    var navLinks = document.getElementById("navLinks");
+
+    function showmenu() {
+      navLinks.style.right = "0"
+    }
+
+    function hidemenu() {
+      navLinks.style.right = "-200px"
+    }
